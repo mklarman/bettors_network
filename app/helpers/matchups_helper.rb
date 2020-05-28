c5e9739550710d0806f56e1c320fd432f054a40c
mklarman <mklarman@hotmail.com>
2020-05-28 12:24:34 -0400

    working on picks

--- app/helpers/matchups_helper.rb
+++ app/helpers/matchups_helper.rb
@@ -4,6 +4,9 @@ module MatchupsHelper
 
 		current_user.selections.each do |s|
 
+			@side_check = false
+			@total_check = false
+
 			if s.matchup_id == @matchup.id
 
 				if s.cat == "side"
@@ -145,5 +148,77 @@ module MatchupsHelper
 
 		end
 
+	end
+
+	def show_games(array_of_objects)
+
+		@games_to_show = []
+
+		array_of_objects.each do |check|
+
+			@matchup_obj = {}
+
+			@matchup_obj[:sport] = check.sport
+			@matchup_obj[:fav] = check.fav
+			@matchup_obj[:dog] = check.dog
+			@matchup_obj[:spread] = check.spread
+			@matchup_obj[:money_line] = check.money_line
+			@matchup_obj[:dog_ml] = check.dog_ml
+			@matchup_obj[:total] = check.total
+
+			if current_user.selections[0]
+
+				current_user.selections.all.each do |sel|
+
+					if sel.matchup_id == check.id
+
+						if sel.choice == "Fav"
+
+							@matchup_obj[:side_or_total] = "side"
+							@matchup_obj[:selection] = check.fav + " " + check.spread.to_s
+
+
+						elsif sel.choice == "Dog"
+
+							@matchup_obj[:side_or_total] = "side"
+							@matchup_obj[:selection] = check.dog + " " + (check.spread.to_f * -1)
+
+
+						elsif sel.choice == "under"
+
+							@matchup_obj[:side_or_total] = "total"
+							@matchup_obj[:selection] = "Under " + check.total.to_s
+
+
+						else sel.choice == "over"
+
+							@matchup_obj[:side_or_total] = "total"
+							@matchup_obj[:selection] = "Over " + check.total.to_s
+
+
+						end
+
+						@games_to_show.push(@matchup_obj)
+
+					else
+
+						@matchup_obj[:side_or_total] = "none"
+						@matchup_obj[:selection] = "none"
+						@games_to_show.push(@matchup_obj)
+
+
+					end
+
+
+				end
+
+			else
+
+				@matchup_obj[:side_or_total] = "none"
+				@matchup_obj[:selection] = "none"
+				@games_to_show.push(@matchup_obj)
+
+			end
+		end
 	end
 end
